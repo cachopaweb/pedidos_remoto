59178a2425b7f37ccec00f8465885449d65b18d9
cachopaweb <cachopaweb@gmail.com>
2024-12-13 17:59:48 -0400

    correções

--- lib/app/controllers/config_controller.dart
+++ lib/app/controllers/config_controller.dart
@@ -2,8 +2,8 @@ class ConfigController {
   static final ConfigController instance = ConfigController._();
 
   String getUrlBase() {
-    // return 'https://servidor-primor-fboxwqyjfq-rj.a.run.app/v1';
-    return 'http://10.0.0.3:3333/v1';
+    return 'https://servidor-primor-fboxwqyjfq-rj.a.run.app/v1';
+    // return 'http://10.0.0.3:3333/v1';
   }
 
   ConfigController._();
